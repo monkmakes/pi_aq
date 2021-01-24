e6c4b90965653018d064bab608db45b6e7221e77
Simon Monk <srmonk@gmail.com>
2021-01-24 16:01:05 +0000

    new proj

--- python/aq_meter_alarm.py
+++ python/aq_meter_alarm.py
@@ -1,16 +1,25 @@
 import threading
 import time
-from guizero import App, Text
+from guizero import App, Text, Slider
 from aq import AQ
 
 aq = AQ()
 
-app = App(title="Air Quality", width=550, height=300, layout="grid")
+app = App(title="Air Quality", width=550, height=400, layout="grid")
 
 def update_readings(): # update fields with new temp and eCO2 readings
     while True:
         temp_c_field.value = str(aq.get_temp())
-        eco2_field.value = str(aq.get_eco2())
+        eco2 = aq.get_eco2()
+        eco2_field.value = str(eco2)
+        if eco2 > slider.value:
+            app.bg = "red"
+            app.text_color = "white"
+            aq.buzzer_on()
+        else:
+            app.bg = "white"
+            app.text_color = "black"
+            aq.buzzer_off()  
         time.sleep(0.5)
 
 t1 = threading.Thread(target=update_readings)
@@ -23,4 +32,6 @@ Text(app, text="Temp (C)", grid=[0,0], size=20)
 temp_c_field = Text(app, text="-", grid=[1,0], size=100)
 Text(app, text="eCO2 (ppm)", grid=[0,1], size=20)
 eco2_field = Text(app, text="-", grid=[1,1], size=100)
+Text(app, text="Alarm (ppm)", grid=[0,2], size=20)
+slider = Slider(app, start=300, end=2000, width=300, height=40, grid=[1,2])
 app.display()
