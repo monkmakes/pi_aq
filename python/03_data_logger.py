36e43e82dcb5e488462eed2f8e6d9b67b04f7ce7
Simon Monk <srmonk@gmail.com>
2021-01-25 08:35:24 +0000

    logger project added

--- python/03_data_logger.py
+++ python/03_data_logger.py
@@ -6,19 +6,28 @@ aq.leds_automatic()
 
 interval = int(input("Enter interval between readings (seconds):"))
 file_name = input("Enter filename:")
+print("Press CTRL-c to end logging")
+
 f = open(file_name, "w")
+f.write("time(s)\ttemp(C)\teCO2(ppm)\n")
+print("time(s)\ttemp(C)\teCO2(ppm)")
 
 last_update = 0
 t0 = int(time.monotonic())
 
-while True:
-    now = time.monotonic()
-    if (now > last_update + interval):
-        last_update = now
-        t = str(int(now) - t0)
-        temp_c = str(aq.get_temp())
-        eco2 = str(int(aq.get_eco2()))
-        f.write(t + "\t")
-        f.write(temp_c + "\t")
-        f.write(eco2 + "\n")
-        print(t + "\t" + temp_c + "\t" + str(int(eco2)))
+try:
+    while True:
+        now = time.monotonic()
+        if (now > last_update + interval):
+            last_update = now
+            t = str(int(now) - t0)
+            temp_c = str(aq.get_temp())
+            eco2 = str(int(aq.get_eco2()))
+            f.write(t + "\t")
+            f.write(temp_c + "\t")
+            f.write(eco2 + "\n")
+            print(t + "\t" + temp_c + "\t" + str(int(eco2)))
+except:
+    f.close()
+    print("\nLogging to file " + file_name + " complete")
+
