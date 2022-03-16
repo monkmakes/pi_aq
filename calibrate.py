0e6d4f409e952ad16150e9bea922bd49b3a96204
simonmonk <evilgeniusauthor@gmail.com>
2022-03-16 12:24:08 +0000

    added calibration facility

--- calibrate.py
+++ calibrate.py
@@ -4,29 +4,33 @@ aq = AQ()
 print("Calibrate sensor")
 print("================")
 print("Only for Air Quaility for Raspbewrry Pi boards of version 1e and later")
-print("Current eCO2 reading: " + str(aq.get_eco2()))
-print("Options")
-print("1. Calibrate to 400 ppm")
-print("2. Reset to factory default")
-option = input(">")
-if (option == '1') :
-    print("Your sensor should be in an environment with around 400ppm CO2 (fresh air)")
-    confirm = input("Calibrate? (Y/N)")
-    if (confirm == 'Y' or confirm == 'y'):
-        aq.calibrate_400()
-        print('Sensor calibrated')
+while True:
+    print("Options")
+    print("1. Show eCO2 reading")
+    print("2. Calibrate to 400 ppm")
+    print("3. Reset to factory default")
+    print("4. Quit")
+    option = input(">")
+    if (option == '1') :
         print("Current eCO2 reading: " + str(aq.get_eco2()))
+    if (option == '2') :
+        print("Your sensor should be in an environment with around 400ppm CO2 (fresh air)")
+        confirm = input("Calibrate? (Y/N)")
+        if (confirm == 'Y' or confirm == 'y'):
+            aq.calibrate_400()
+            print('Sensor calibrated')
+        else:
+            print('Cancelled calibration')
+    elif (option == '3') :
+        confirm = input("Reset calibration to factory default? (Y/N)")
+        if (confirm == 'Y' or confirm == 'y'):
+            aq.reset_calibration()
+            print('Sensor set to factory default')
+        else:
+            print('Cancelled calibration')
+    elif (option == '4') :
+        print("Bye")
+        break
     else:
-        print('Cancelled calibration')
-elif (option == '2') :
-    confirm = input("Reset calibration to factory default? (Y/N)")
-    if (confirm == 'Y' or confirm == 'y'):
-        aq.reset_calibration()
-        print('Sensor set to factory default')
-        print("Current eCO2 reading: " + str(aq.get_eco2()))
-    else:
-        print('Cancelled calibration')
-else:
-    print('unknown option')
+        print('unknown option')
 
-aq.leds_automatic()
